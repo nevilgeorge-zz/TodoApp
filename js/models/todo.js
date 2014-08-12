46136e5dd01802b989e19af9dba3544768701cc2
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-08-11 23:24:25 -0700

    Todo model created

--- js/models/todo.js
+++ js/models/todo.js
@@ -1,1 +1,23 @@
 // todo.js
+
+var app = app || {};
+
+// Todo model. Wrapping it in a namespace so we don't clutter the global scope.
+
+app.Todo = Backbone.Model.extend({
+
+	// Default attributes ensure that each todo crated has 'title' and 'completed' keys
+	defaults: {
+		title: '',
+		completed: false
+	},
+
+	// Function to toggle whether or not a todo item is completed or not
+	// Updates and saves in one function
+	toggle: function() {
+		this.save({
+			completed: !this.get('completed')
+		});
+	}
+
+})
