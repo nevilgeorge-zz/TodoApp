2295582ee2a7ae47c070fd8b2b355f85c12f7c7d
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-08-14 20:34:09 -0700

    created router

--- js/routers/router.js
+++ js/routers/router.js
@@ -1,1 +1,24 @@
-// router.js
+// js/routers/router.js
+
+// Todo Router.
+/* We will have 3 routes:
+	#/ (all/ default)
+	#/active
+	$/completed
+*/
+
+var Workspace = Backbone.Router.extend({
+
+	routes: {
+		'*filter': 'setFilter'
+	},
+
+	setFilter: function(param) {
+		// Set the filter to be used
+		// Trigger a collection filter event, causing hiding/unhiding of Todo view items
+		window.app.Todos.trigger('filter');
+	}
+});
+
+app.TodoRouter = new Workspace();
+Backbone.history.start();
